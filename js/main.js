8422371c767229178254f1cd19c2775c972fee50
Julien Benatar <contact@julienbenatar.com>
2015-02-15 12:20:51 -0500

    clean main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,5 @@
 angular.module('jb', ['ngAnimate']);
 
-// app.config(['$routeProvider',
-	// function($routeProvider) {
-	// 	$routeProvider.
-	// 	when('/music', {
-	// 		templateUrl: 'partials/phone-list.html',
-	// 		controller: 'PhoneListCtrl'
-	// 	}).
-	// 	when('/phones/:phoneId', {
-	// 		templateUrl: 'partials/phone-detail.html',
-	// 		controller: 'PhoneDetailCtrl'
-	// 	}).
-	// 	otherwise({
-	// 		redirectTo: '/phones'
-	// 	});
-	// }
-// ]);
-
 angular.module('jb').directive('jbBackgroundVideo', function() {
 	'use strict';
 	return {
