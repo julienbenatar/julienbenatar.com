4bb05419400d4b00e80c3bffe7dd1f96994b5134
Julien Benatar <julien@nextbigsound.com>
2015-09-20 15:44:54 -0400

    add animation after menu component is loaded

--- app/components/indexComponent/menu/menu.js
+++ app/components/indexComponent/menu/menu.js
@@ -1,7 +1,24 @@
 var MenuComponent = React.createClass({
+	getInitialState: function() {
+		return {
+			visible: false
+		};
+	},
+
+	componentDidMount: function() {
+		var that = this;
+		setTimeout(function() {
+			that.setState({
+				visible: true
+			});
+		}, 200);
+	},
+
 	render: function() {
 		return (
-			<div className="menuComponent">
+			<div
+				className={this.state.visible ? 'menuComponent -visible' : 'menuComponent'}
+			>
 				<div className="menu__logo"></div>
 				<ul className="menuList">
 					<li className="menuItem">
