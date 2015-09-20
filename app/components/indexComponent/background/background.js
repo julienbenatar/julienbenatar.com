a6841d4618eae280fc84ecb15f7257d5635d88f6
Julien Benatar <julien@nextbigsound.com>
2015-09-20 14:29:22 -0400

    add controller into background class

--- app/components/indexComponent/background/background.js
+++ app/components/indexComponent/background/background.js
@@ -1,4 +1,51 @@
 var BackgroundComponent = React.createClass({
+
+	getInitialState: function() {
+		return {
+			isLoaded: false,
+			isPlaying: true
+		};
+	},
+
+	componentDidMount: function() {
+		var that = this;
+		var video = React.findDOMNode(this.refs.video_tag);
+		video.oncanplaythrough = function() {
+			that.state.isLoaded = true;
+			video.loop = true;
+			that.playVideo();
+		};
+
+		video.onended = function() {
+			that.playVideo();
+		}
+	},
+
+	playVideo: function() {
+		var video = React.findDOMNode(this.refs.video_tag);
+		this.setState({
+			isPlaying: true
+		});
+		video.play();
+	},
+
+	pauseVideo: function() {
+		var video = React.findDOMNode(this.refs.video_tag);
+		this.setState({
+			isPlaying: false
+		});
+		video.pause();
+	},
+
+	clickButton: function() {
+		if (this.state.isPlaying === true) {
+			this.pauseVideo();
+		}
+		else {
+			this.playVideo();
+		}
+	},
+
 	render: function() {
 		return (
 			<div className="backgroundComponent">
@@ -9,7 +56,8 @@ var BackgroundComponent = React.createClass({
 				<video
 					className="backgroundVideo"
 					loop="loop"
-					autoPlay="autoplay"
+					ref="video_tag"
+					play="{this.state.isLoaded === 1}"
 				>
 					<source
 						src="assets/images/background/background.mp4"
@@ -24,8 +72,10 @@ var BackgroundComponent = React.createClass({
 						type="video/webm"
 					/>
 				</video>
-				<div className="playerButton playerButton--play"></div>
-				<div className="playerButton playerButton--contrast"></div>
+				<div
+					onClick={this.clickButton}
+					className={this.state.isPlaying ? 'playerButton -typePause' : 'playerButton -typePlay'}
+				></div>
 			</div>
 		);
 	}
